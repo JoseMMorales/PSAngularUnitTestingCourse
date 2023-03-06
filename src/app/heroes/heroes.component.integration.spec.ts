3c62711619b965c88858933ea3f0975d9f999720
JoseMMorales <josemulamorales33@hotmail.com>
2023-03-06 12:09:35 +0100

    Triggering Events on Elements

--- src/app/heroes/heroes.component.integration.spec.ts
+++ src/app/heroes/heroes.component.integration.spec.ts
@@ -59,4 +59,20 @@ describe("HeroesComponent (integration tests)", () => {
       expect(heroComponent[i].componentInstance.hero).toEqual(HEROES[i]);
     });
   });
+
+  it("Should call heroService.deleteHero when the Hero Component delete button is clicked", () => {
+    spyOn(fixture.componentInstance, "delete");
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(
+      By.directive(HeroComponent)
+    );
+    heroComponents[0]
+      .query(By.css("button"))
+      .triggerEventHandler("click", { stopPropagation: () => {} });
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  });
 });
